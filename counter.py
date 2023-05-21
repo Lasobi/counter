385d70e0f3ecf4e90db021eac2233c3ab12f5217
Lars Birkeland <30419055+Lasobi@users.noreply.github.com>
2023-05-21 12:45:38 +0200

    Added a test file

--- counter.py
+++ counter.py
@@ -3,7 +3,7 @@ from tkinter import ttk
 import argparse
 
 class Counter:
-    def __init__(self, master, name, size, initial_count):
+    def __init__(self, master, name, initial_count):
         self.master = master
         self.name = name
         self.count = initial_count
@@ -42,25 +42,18 @@ class Counter:
 def main():
     parser = argparse.ArgumentParser(description='A program to create multiple counters')
     parser.add_argument('names', metavar='name', type=str, nargs='*', help='a list of names for the counters')
-    parser.add_argument('-s', '--size', type=int, default=100, help='the size of the counters')
     parser.add_argument('-c', '--count', type=int, metavar='count_value', nargs='+', default=[], help='the initial count value(s) for the counters')
     args = parser.parse_args()
 
     root = tk.Tk()
     root.title('Counters')
 
-    counters_height = len(args.names) * 60  # Adjust the height based on the number of counters
-    min_window_width = args.size
-    min_window_height = counters_height + 100  # Add extra padding for window borders and controls
-
-    root.minsize(min_window_width, min_window_height)
-
     if not args.names:
-        Counter(root, "", args.size, 0)
+        Counter(root, "", 0)
     else:
         for i, name in enumerate(args.names):
             initial_count = args.count[i] if i < len(args.count) else 0
-            Counter(root, name, args.size, initial_count)
+            Counter(root, name, initial_count)
 
     root.mainloop()
 
---
+++ counter_test.py
@@ -0,0 +1,22 @@
+import unittest
+from counter import Counter
+import tkinter as tk
+
+class TestCounter(unittest.TestCase):
+
+    def setUp(self):
+        self.root = tk.Tk()
+        self.counter = Counter(self.root, "TestCounter", 0)
+
+    def test_increment(self):
+        self.counter.increment()
+        self.assertEqual(self.counter.count, 1)
+
+    def test_decrement(self):
+        self.counter.increment()
+        self.counter.increment()
+        self.counter.decrement()
+        self.assertEqual(self.counter.count, 1)
+
+if __name__ == '__main__':
+    unittest.main()
